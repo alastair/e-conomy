acd51b853df0ea3d3abb24799722d474ecb02b23
Alastair Porter <alastair@porter.net.nz>
2011-09-18 18:22:19 -0400

    Users/login

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+
+from google.appengine.api import users
+import cgi
+import logging
 
 import os
 import os.path
@@ -30,12 +20,49 @@ class RenderedHandler(webapp.RequestHandler):
 
 class MainHandler(RenderedHandler):
     def get(self):
-        data = {"money": 100}
+        user = users.get_current_user()
+
+        n = ""
+        if user:
+            q = db.GqlQuery("SELECT * FROM Player WHERE user = :1", user)
+            thisuser = q.get()
+            if not thisuser:
+                thisuser = Player()
+                thisuser.put()
+            if not thisuser.name:
+                n = user
+            else:
+                n = thisuser.name
+        data = {"money": 100,
+                "username": n,
+                "login_url": users.create_login_url(self.request.uri),
+                "logout_url": users.create_logout_url(self.request.uri),
+                "user": user
+                }
         self.render("index.html", data)
 
+# Handler to get name change information, store it, and redirect back to the homepage
+class UserHandler(RenderedHandler):
+    def post(self):
+        user = users.get_current_user()
+        if not user:
+            logging.debug("redirect")
+            self.redirect(users.create_login_url(self.request.uri))
+        user = users.get_current_user()
+        playerlist = Player.all()
+        playerlist.filter("user =", user)
+        thisplayer = playerlist.get()
+        thisplayer.name = cgi.escape(self.request.get("username"))
+        logging.debug("playername: %s", thisplayer.name)
+        thisplayer.put()
+        self.redirect("/")
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+    handlers = [
+        ("/user", UserHandler),
+        ("/", MainHandler)
+    ]
+    application = webapp.WSGIApplication(handlers,
                                          debug=True)
     util.run_wsgi_app(application)
 
